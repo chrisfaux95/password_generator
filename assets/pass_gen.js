afa9ab9abba16b8f1bfee8984cd70d8764aa8f54
Chris Faux <chrisfaux95@gmail.com>
2020-07-21 16:36:51 -0400

    working version

--- assets/pass_gen.js
+++ assets/pass_gen.js
@@ -12,3 +12,40 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//constants
+const alphaLower = "abcdefghijklmnopqrstuvwxyz";
+const alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const alphaNumer = "1234567890";
+const alphaSpecial =  "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
+//password generator
+function generatePassword() {
+    let passwordLength = parseInt(prompt("How Long Would You Like Your Password?"))
+    let allowLower = confirm("Allow Lowercase?");
+    let allowUpper = confirm("Allow Uppercase?");
+    let allowNumber = confirm("Allow Numbers?");
+    let allowSpecial = confirm("Allow Special Characters?");
+
+    let allowedString = "";
+
+    if (allowLower) {
+        allowedString += alphaLower;
+    }
+    if (allowUpper) {
+        allowedString += alphaUpper;
+    }
+    if (allowNumber) {
+        allowedString += alphaNumer;
+    }
+    if (allowSpecial) {
+        allowedString += alphaSpecial;
+    }
+    // console.log(allowedString);
+    let passString = "";
+    for (let i = 0; i < passwordLength; i++) {
+        passString += allowedString[Math.floor(Math.random() * allowedString.length)]
+        console.log(passString);
+    }
+    return passString;
+}
